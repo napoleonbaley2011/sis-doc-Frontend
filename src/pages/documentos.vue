3e1dd2a3b42a7aeb7d2d5f72f65a2e584d0efe56
KatRose7 <rgonzalesch@fcpn.edu.bo>
2024-04-15 13:32:50 -0400

    coodigo nuevo

---
+++ src/pages/documentos.vue
@@ -0,0 +1,139 @@
+<template>
+
+    <nav>
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Documentos</a></li>
+            <li><a href="#">Seguimiento</a></li>
+            <li><a href="#">Configuraciones</a></li>
+        </ul>
+    </nav>
+    <body>
+        <!--   Tarjetas-->
+    <div class="title-cards">
+            <h2>DOCUMENTOS</h2>
+        </div>
+    <div class="container-card">
+        
+    <div class="card">
+        <figure>
+            <img src="">
+        </figure>
+        <div class="contenido-card">
+            <h3>PLANEAR</h3>       
+            <a href="lista.html">Opcion</a>
+        </div>
+    </div>
+    <div class="card">
+        <figure>
+            <img src="">
+        </figure>
+        <div class="contenido-card">
+            <h3>HACER</h3>
+            <a href="#">Opcion</a>
+        </div>
+    </div>
+    <div class="card">
+        <figure>
+            <img src="">
+        </figure>
+        <div class="contenido-card">
+            <h3>VERIFICAR</h3>
+            <a href="#">Opcion</a>
+        </div>
+    </div>
+    <div class="card">
+        <figure>
+            <img src="">
+        </figure>
+        <div class="contenido-card">
+            <h3>ACTUAR</h3>
+            <a href="#">Opcion</a>
+        </div>
+    </div>
+    </div>
+
+
+</body>
+</template>
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+}
+.container-card{
+	width: 100%;
+	display: flex;
+	max-width: 1100px;
+	margin: auto;
+}
+.title-cards{
+	width: 100%;
+	max-width: 1080px;
+	margin: auto;
+	padding: 20px;
+	margin-top: 20px;
+	text-align: center;
+	color: #7a7a7a;
+}
+.card{
+	width: 100%;
+	margin: 20px;
+	border-radius: 6px;
+	overflow: hidden;
+	background:#fff;
+	box-shadow: 0px 1px 10px rgba(0,0,0,0.2);
+	transition: all 400ms ease-out;
+	cursor: default;
+}
+.card:hover{
+	box-shadow: 5px 5px 20px rgba(0,0,0,0.4);
+	transform: translateY(-3%);
+}
+.card img{
+	width: 100%;
+	height: 210px;
+}
+.card .contenido-card{
+	padding: 15px;
+	text-align: center;
+}
+.card .contenido-card h3{
+	margin-bottom: 15px;
+	color: #7a7a7a;
+}
+.card .contenido-card p{
+	line-height: 1.8;
+	color: #6a6a6a;
+	font-size: 14px;
+	margin-bottom: 5px;
+}
+.card .contenido-card a{
+	display: inline-block;
+	padding: 10px;
+	margin-top: 10px;
+	text-decoration: none;
+	color: #000;
+	border: 1px solid #000;
+	border-radius: 4px;
+	transition: all 400ms ease;
+	margin-bottom: 5px;
+}
+.card .contenido-card a:hover{
+	background: #F5ff68;
+	color: #fff;
+}
+@media only screen and (min-width:320px) and (max-width:768px){
+	.container-card{
+		flex-wrap: wrap;
+	}
+	.card{
+		margin: 15px;
+	}
+}
+
+
+</style>
---
+++ src/pages/lista.vue
@@ -0,0 +1,112 @@
+<template>
+    <body>
+    <nav>
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Documentos</a></li>
+            <li><a href="#">Seguimiento</a></li>
+            <li><a href="#">Configuraciones</a></li>
+        </ul>
+    </nav>
+    <body>
+        <h1>PLANEAR</h1>
+        <table class="tabla-opciones">
+            <tbody>
+                <tr>
+                    <td>Analisis FODA</td>
+                    <td>
+                        <button class="btn-abrir-formulario">Abrir formulario</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Mapa de Procesos</td>
+                    <td>
+                        <button class="btn-abrir-formulario">Abrir formulario</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Matriz de partes interesadas</td>
+                    <td>
+                        <button class="btn-abrir-formulario">Abrir formulario</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Alcance</td>
+                    <td>
+                        <button class="btn-abrir-formulario">Abrir formulario</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Politica</td>
+                    <td>
+                        <button class="btn-abrir-formulario">Abrir formulario</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Evidencia de liderazgo y compromiso</td>
+                    <td>
+                        <button class="btn-abrir-formulario">Abrir formulario</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Matriz de riesgos y oportunidades</td>
+                    <td>
+                        <button class="btn-abrir-formulario">Abrir formulario</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Roles y responsabilidades</td>
+                    <td>
+                        <button class="btn-abrir-formulario">Abrir formulario</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Matriz IPER</td>
+                    <td>
+                        <button class="btn-abrir-formulario">Abrir formulario</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Matriz de requisitos legales</td>
+                    <td>
+                        <button class="btn-abrir-formulario">Abrir formulario</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Planificacion de objetivos</td>
+                    <td>
+                        <button class="btn-abrir-formulario">Abrir formulario</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+      
+      </body>
+</body>
+</template>
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+}
+.tabla-opciones {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+.tabla-opciones th,
+.tabla-opciones td {
+    border: 1px solid #ddd;
+    padding: 8px;
+    text-align: left;
+}
+
+.tabla-opciones i {
+    margin-right: 5px;
+}
+
+
+</style>
